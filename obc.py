990350f17f2ab6f662d70b09cbd32aa0443b53ad
Blake Putnam <OozeSecurity@protonmail.com>
2023-09-24 18:01:04 -0500

    added recon folder, fixed obc.py pathing
    recon folder should now get downloaded when installed.

--- obc.py
+++ obc.py
@@ -10,17 +10,17 @@ while True:
         break
     elif ui == "1" or ui == 1:
         a = print('\n----------------------\n> Orbital Beam Online & Ready\n-----------------------')
-        b = sh('echo "> Target Range?\n " && read ip && echo "> Target Name?\n" && read target && sudo nmap -A -O -n -p- -oN "~/.obc/obc/recon/$target.txt" $ip > /dev/null 2>&1')
+        b = sh('echo "> Target Range?\n " && read ip && echo "> Target Name?\n" && read target && sudo nmap -A -O -n -p- -oN "~/.obc/recon/$target.txt" $ip > /dev/null 2>&1')
         c = print('\n----------------------\n> Target Recon Completed.\n')
         d = print('\n----------------------\n> O.B.C. Terminal\n----------------------\n> OPTIONS:\n> 1: Fingerprint\n> 2: PortScan\n> 3: Vulncannon\n> 4: Exit\n')
     elif ui == "2" or ui == 2:
         a = print('\n----------------------\n> Orbital Beam Online & Ready\n-----------------------')
-        b = sh('echo "> Target Range?\n " && read ip && echo "> Target Name?\n" && read target && sudo nmap -p0-65535 -oN "~/.obc/obc/recon/$target.txt" $ip > /dev/null 2>&1')
+        b = sh('echo "> Target Range?\n " && read ip && echo "> Target Name?\n" && read target && sudo nmap -p0-65535 -oN "~/.obc/recon/$target.txt" $ip > /dev/null 2>&1')
         c = print('\n----------------------\n> Target(s) Identified Completed.\n')
         d = print('\n----------------------\n> O.B.C. Terminal\n----------------------\n> OPTIONS:\n> 1: Fingerprint\n> 2: PortScan\n> 3: Vulncannon\n> 4: Exit\n')
     elif ui == "3" or ui == 3:
         a = print('\n----------------------\n> Orbital Beam Online & Ready\n-----------------------')
-        b = sh('echo "> Target?\n " && read ip && echo "> Target Name?\n" && read target && sudo nmap --script vuln -oN "~/.obc/obc/recon/$target.txt" $ip > /dev/null 2>&1')
+        b = sh('echo "> Target?\n " && read ip && echo "> Target Name?\n" && read target && sudo nmap --script vuln -oN "~/.obc/recon/$target.txt" $ip > /dev/null 2>&1')
         c = print('\n----------------------\n> Cannon Fired.\n')
         d = print('\n----------------------\n> O.B.C. Terminal\n----------------------\n> OPTIONS:\n> 1: Fingerprint\n> 2: PortScan\n> 3: Vulncannon\n> 4: Exit\n')
     else:
