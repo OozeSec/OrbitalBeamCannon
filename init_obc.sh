05684c5c6928e84fc8a357f7467743ba6431b6ce
Blake Putnam <79871998+OozeSec@users.noreply.github.com>
2023-09-24 16:54:19 -0500

    Update init_obc.sh

--- init_obc.sh
+++ init_obc.sh
@@ -7,19 +7,17 @@ APT="$(cat $OSTEMP | grep Ubuntu | wc -l)"
 DNF="$(cat $OSTEMP | grep 'Fedora\|CentOS\|RHEL' | wc -l)" 
 
 if [ $APT -eq 1 ] ; then
-#    sudo apt install nmap -y
-APT_NMAP='sudo apt install nmap -y'   
+APT_NMAP="$(sudo apt-get install nmap -y)"
     $APT_NMAP
     mkdir ~/.obc
-    git clone -b stable --single-branch git@github.com:OozeSec/OrbitalBeamCannon.git [~/.obc]
-    echo "alias obc='~/.obc/obc.py'.." >> ~/.bashrc && source ~/.bashrc
+    git clone -b stable --single-branch https://github.com/OozeSec/OrbitalBeamCannon.git ~/.obc
+    echo "alias obc='python3 ~/.obc/obc.py'" >> ~/.bashrc && source ~/.bashrc
     echo "Orbital Beam Cannon Now Online & Operational."
-elif grep -q -- 1 "$DNF" ; then
-#    dnf install nmap -y >> /dev/null 2&>1
+elif [ $DNF -eq 1 ] ; then
 DNF_NMAP='sudo dnf install nmap -y'
     $DNF_NMAP
     mkdir ~/.obc
-    git clone -b stable --single-branch git@github.com:OozeSec/OrbitalBeamCannon.git [~/.obc]
-    echo "alias obc='~/.obc/obc.py'.." >> ~/.bashrc && source ~/.bashrc
+    git clone -b stable --single-branch https://github.com/OozeSec/OrbitalBeamCannon.git ~/.obc
+    echo "alias obc='python3 ~/.obc/obc.py'" >> ~/.bashrc && source ~/.bashrc
     echo "Orbital Beam Cannon Now Online & Operational."
 fi
